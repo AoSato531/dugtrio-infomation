bf9a8aab377898e1d2e971b63a4d14bd840626dd
Hinako Dug <>
2024-03-28 11:33:43 +0900

    おあそびコミット

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+      <div className="footer">
+        <div>管理人</div>
+        <div>だぐ</div>
+      </div>
+      
+    );
+  }
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+      <div className="header">
+        <h1>ダグトリオ情報サイト</h1>
+        <div>ヘッダ</div>
+      </div>
+      
+    );
+  }
